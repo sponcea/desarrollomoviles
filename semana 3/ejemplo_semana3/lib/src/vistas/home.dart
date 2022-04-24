0c95cc7278d53c40caf816baed1d4685d2ba6dea
elserch87 <81885018+elserch87@users.noreply.github.com>
2022-04-23 22:06:33 -0500

    continuación del ejemplo de la semana 3, agregamos, Drawer, Elevated Button, ListView y Listile

--- semana 3/ejemplo_semana3/lib/src/vistas/home.dart
+++ semana 3/ejemplo_semana3/lib/src/vistas/home.dart
@@ -8,24 +8,54 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   Color _originalTextColorBody = Colors.black;
   Color _originalBackgroundColorBody = Colors.white;
+  Color _originalBackgroundColorAppbar = Colors.blue;
 
   Color _darkModeTextColorBody = Colors.white;
   Color _darkModeBackgroundColorBody = Colors.grey.shade900;
-
+  Color _darkModeBackgroundColorAppbar = Colors.black;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Ejemplo semana 3')),
+      drawer: Drawer(
+          child: ListView(
+        children: [
+          ListTile(
+            title: Text('item 1'),
+            leading: Icon(Icons.ac_unit),
+          ),
+          ListTile(
+            title: Text('item 2'),
+          )
+        ],
+      )),
+      appBar: AppBar(
+        title: const Text('Ejemplo semana 3/4'),
+        backgroundColor: _originalBackgroundColorAppbar,
+      ),
       body: Center(
           child: Text(
-        'Hola!!!',
+        'Helou!!!',
         style: TextStyle(color: _originalTextColorBody),
       )),
       backgroundColor: _originalBackgroundColorBody,
-      floatingActionButton: FloatingActionButton(
-        onPressed: _modoOscuro,
-        child: const Icon(Icons.dark_mode),
-      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: _botonesFlotantes(),
+      persistentFooterButtons: [
+        ElevatedButton(
+          onPressed: () {
+            print("Un botón persistente 1");
+          },
+          child: Icon(Icons.ac_unit_outlined),
+          style: ElevatedButton.styleFrom(primary: Colors.red),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            print("Un botón persistente 2");
+          },
+          child: Icon(Icons.access_alarm),
+          style: ElevatedButton.styleFrom(primary: Colors.green),
+        )
+      ],
     );
   }
 
@@ -33,7 +63,26 @@ class _HomeState extends State<Home> {
     setState(() {
       _originalTextColorBody = _darkModeTextColorBody;
       _originalBackgroundColorBody = _darkModeBackgroundColorBody;
+      _originalBackgroundColorAppbar = _darkModeBackgroundColorAppbar;
       print('Clic en botón dark mode');
     });
+  }
+
+  void _modoClaro() {}
+
+  Widget _botonesFlotantes() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        FloatingActionButton(
+          onPressed: _modoOscuro,
+          child: const Icon(Icons.dark_mode),
+        ),
+        FloatingActionButton(
+          onPressed: _modoClaro,
+          child: const Icon(Icons.light_mode),
+        ),
+      ],
+    );
   }
 }
